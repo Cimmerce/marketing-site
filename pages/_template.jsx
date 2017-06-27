abbcc93803334e94a3f900cff509bbeeb3dafcd2
Casper Fabricius <me@casperfabricius.com>
2017-06-27 11:13:44 +0200

    Restructure style sheets a bit.

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 // Reset stylesheet
 require('normalize.css')
+require('../styles/base/base.scss')
 
 class Template extends React.Component {
   render () {
     const { children } = this.props
 
     return (
-      <div className='wrapper'>
+      <div>
         {children}
       </div>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,57 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import cx from 'classnames'
 import Helmet from 'react-helmet'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 
 import { config } from 'config'
+import sectionStyles from '../styles/base/section.scss'
 import styles from '../styles/index.scss'
 
-export default class Index extends React.Component {
+export default class Index extends Component {
   render () {
     return (
-      <div className={cx(['main', styles.test])}>
-        <div className='main-container'>
-          <Helmet title={config.siteTitle} meta={[{ 'name': 'description', 'content': 'sample' }]} />
+      <div>
+        <Helmet
+          title={`${config.siteTitle} - ${config.siteDescription}`}
+          meta={[{ name: 'description', content: config.siteDescription }]}
+        />
 
-          <div className='logo centre'>
-            <div className='logo-outerCircle'>
-              <div className='logo-outerCircle-triangle'>
-                <div className='logo-outerCircle-triangle-square'>
-                  <div className='logo-outerCircle-triangle-square-innerCircle' />
-                </div>
-              </div>
+        <section className={cx([sectionStyles.section, sectionStyles.contrastSection])}>
+          <div className={styles.leadTexts}>
+            <div className={styles.bigLeadText}>
+              3D and Virtual Reality for ecommerce
+            </div>
+            <div className={styles.subLeadText}>
+              Cimmerce lets your business increase conversion and decrease returns
+              with web-based 3D visualization and Virtual Reality that plugs right
+              into your website.
             </div>
           </div>
-
-          <div className='main-container-title'>
-            <h1>Alchemy</h1>
-          </div>
-
-          <div className='main-container-subtitle'>
-            <h3>A Gatsby starter with PostCSS powers</h3>
-          </div>
-
-          <div className='main-container-links'>
-            <ul>
-              <li>
-                <Link to={prefixLink('/philosophy/')}>Philosophy</Link>
-              </li>
-              <li>
-                <Link to={prefixLink('/articles/')}>Articles</Link>
-              </li>
-              <li>
-                <a href='https://github.com/bntzio/gatsby-starter-alchemy' target='_blank'>Github</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className='main-footer'>
-          <p>
-            by <a href='https://twitter.com/bntzio' target='_blank'>@bntzio</a>
-          </p>
-        </div>
+        </section>
       </div>
     )
   }
