482be8fccb14a5a43ba0435fdac95340cb6a7e05
Casper Fabricius <me@casperfabricius.com>
2017-06-28 13:47:52 +0200

    Changed css modules to generate more readable class names. Only use postcss to apply autoprefixer.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,27 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const cssnested = require('postcss-nested')
-const atImport = require('postcss-import')
+const autoprefixer = require('autoprefixer')
 
 exports.modifyWebpackConfig = function (config, env) {
   config.removeLoader('sass')
 
+  const cssModuleLoaderConfig = 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize'
+
   if (env === 'develop') {
     config.loader('sass', {
       test: /\.(sass|scss$)/,
-      loaders: ['style', 'css?modules', 'sass']
+      loaders: ['style', cssModuleLoaderConfig, 'postcss', 'sass']
     })
   } else {
     config.loader('sass', {
       test: /\.(sass|scss)$/,
-      loader: ExtractTextPlugin.extract('style', ['css?modules', 'postcss', 'sass'])
+      loader: ExtractTextPlugin.extract(
+        'style', [cssModuleLoaderConfig, 'postcss', 'sass']
+      )
     })
   }
 
   config.merge({
-    postcss: [
-      atImport(),
-      cssnested
-    ]
+    postcss: [autoprefixer]
   })
 
   return config
