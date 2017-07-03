13f512636436dca9e360bc1a2bc87436b8d7b8e6
Casper Fabricius <me@casperfabricius.com>
2017-07-03 14:15:59 +0200

    Added icons to How we are different section.

--- components/HowWeAreDifferentSection/HowWeAreDifferentSection.jsx
+++ components/HowWeAreDifferentSection/HowWeAreDifferentSection.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import cx from 'classnames'
 
+import cartImageSrc from './cart-arrow-down.svg'
+import cloudImageSrc from './cloud.svg'
+import chromeImageSrc from './chrome.svg'
+import diamondImageSrc from './diamond.svg'
+import edgeImageSrc from './edge.svg'
+import eyeImageSrc from './eye.svg'
+import firefoxImageSrc from './firefox.svg'
+import safariImageSrc from './safari.svg'
+
 import sectionStyles from '../../styles/base/section.scss'
 import styles from './HowWeAreDifferentSection.scss'
 
@@ -14,6 +23,7 @@ const HowWeAreDifferentSection = () => {
 
         <div className={sectionStyles.list}>
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
+            <img src={diamondImageSrc} className={sectionStyles.listIcon} />
             <div className={sectionStyles.listItemHeader}>
               True 3D models
             </div>
@@ -23,15 +33,21 @@ const HowWeAreDifferentSection = () => {
           </div>
 
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
+            <img src={cloudImageSrc} className={sectionStyles.listIcon} />
             <div className={sectionStyles.listItemHeader}>
               Web-based Virtual Reality
             </div>
             <div className={sectionStyles.listItemBody}>
-              Almost any other VR solution requires the customer to download an app to their device. This is a major obstacle to conversion, and one our instantly available web-based VR solution completely eliminates.
+              Downloading an app is a major obstacle to conversion. Our instantly available web-based VR solution runs directly in the browser.
             </div>
+            <img src={chromeImageSrc} className={sectionStyles.bottomIcon} />
+            <img src={firefoxImageSrc} className={sectionStyles.bottomIcon} />
+            <img src={edgeImageSrc} className={sectionStyles.bottomIcon} />
+            <img src={safariImageSrc} className={sectionStyles.bottomIcon} />
           </div>
 
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
+            <img src={eyeImageSrc} className={sectionStyles.listIcon} />
             <div className={sectionStyles.listItemHeader}>
               Highest degree of realism
             </div>
@@ -41,6 +57,7 @@ const HowWeAreDifferentSection = () => {
           </div>
 
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
+            <img src={cartImageSrc} className={sectionStyles.listIcon} />
             <div className={sectionStyles.listItemHeader}>
               Buy from Virtual Reality
             </div>
