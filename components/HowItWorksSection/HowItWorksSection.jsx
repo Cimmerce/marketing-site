c1f149f1c94c5a02e8ed73a53dc070ce6fbb358a
Casper Fabricius <me@casperfabricius.com>
2017-07-03 13:43:59 +0200

    Added icons to How it works section.

--- components/HowItWorksSection/HowItWorksSection.jsx
+++ components/HowItWorksSection/HowItWorksSection.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import cx from 'classnames'
 
+import laptopImageSrc from './laptop.svg'
+import mobileImageSrc from './mobile.svg'
+import snowflakeImageSrc from './snowflake-o.svg'
+import tabletImageSrc from './tablet.svg'
+import televisionImageSrc from './television.svg'
+
+import cardboardImageSrc from './cardboard.png'
+import daydreamImageSrc from './daydream.png'
+import oculusImageSrc from './oculus.png'
+import viveImageSrc from './vive.png'
+
 import sectionStyles from '../../styles/base/section.scss'
 import styles from './HowItWorksSection.scss'
 
@@ -20,6 +31,7 @@ const HowItWorksSection = () => {
             <div className={sectionStyles.listItemBody}>
               We help you digitize your products into highly realistic, true 3D models.
             </div>
+            <img src={snowflakeImageSrc} className={styles.centerIcon} />
           </div>
 
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
@@ -29,6 +41,10 @@ const HowItWorksSection = () => {
             <div className={sectionStyles.listItemBody}>
               We provide the platform that lets your customers see your products in 3D on any device with a web browser.
             </div>
+            <img src={laptopImageSrc} className={styles.bottomIcon} />
+            <img src={mobileImageSrc} className={styles.bottomIcon} />
+            <img src={tabletImageSrc} className={styles.bottomIcon} />
+            <img src={televisionImageSrc} className={styles.bottomIcon} />
           </div>
 
           <div className={cx(sectionStyles.listItem, styles.listItem)}>
@@ -38,6 +54,11 @@ const HowItWorksSection = () => {
             <div className={sectionStyles.listItemBody}>
               Our platform can give your customers a brand new and completely immersive experience exploring your products in virtual reality.
             </div>
+            <img src={cardboardImageSrc} className={styles.bottomIcon} />
+            <img src={daydreamImageSrc} className={styles.bottomIcon} />
+            <img src={oculusImageSrc} className={styles.bottomIcon} />
+            <img src={viveImageSrc} className={styles.bottomIcon} />
+
           </div>
         </div>
       </div>
