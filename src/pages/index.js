dcd905e79ec9cbc68e2721af254ec0c662c86898
ybinstock <ybinstock@gmail.com>
2018-02-15 09:58:01 +0100

    added reactstrap to front page and made relevant changes to the divs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import cx from 'classnames'
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap'
+
 
 import videoPosterSrc from '../assets/testvideo.jpg'
 import videoMp4Src from '../assets/testvideo.mp4'
@@ -16,10 +22,10 @@ const IndexPage = () => (
         <source src={videoWebMSrc} type="video/webm" />
       </video>
 
-      <div className={cx('container', 'text-center', styles.test)}>
+      <Container className={cx('text-center', styles.test)}>
 
-        <div className="row h-full">
-          <div className="col-12 col-lg-8 offset-lg-2 align-self-center pt-150">
+        <Row className="h-full">
+          <Col className="col-12 col-lg-8 offset-lg-2 align-self-center pt-150">
 
             <h1 className="display-4 fw-100 hidden-sm-down">AR-enabled mobile web sales</h1>
             <h1 className="fw-100 hidden-md-up">Integrates right into your ecommerce solution</h1>
@@ -29,27 +35,30 @@ const IndexPage = () => (
             <br />
             <p className="fs-12"><span className="opacity-60 mr-8">Or</span> <a className="text-white mark-border" href="#">Purchase Now</a></p>
 
-          </div>
+          </Col>
+
+          // <div className={styles.test}> This is for including styling without needed to merge style guides</div>
+          
 
           <div className="col-12 align-self-end text-center pb-50">
             <a className="scroll-down-1 scroll-down-inverse" href="#" data-scrollto="section-intro"><span></span></a>
           </div>
 
-        </div>
+        </Row>
 
-      </div>
+      </Container>
     </header>
 
     <main className="main-content">
       <section className="section" id="section-intro">
-        <div className="container">
+        <Container className="">
           <header className="section-header">
             <small>Headers</small>
             <h2>More Headers</h2>
             <hr />
             <p className="lead">TheSaaS includes several header examples which can be use just by copy and paste the code</p>
           </header>
-        </div>
+        </Container>
       </section>
     </main>
   </div>
