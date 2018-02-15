f9497e145a0e5a25c4dbd9ec035b251ccbfb30f3
ybinstock <ybinstock@gmail.com>
2018-02-15 14:45:49 +0100

    added content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import Link from 'gatsby-link'
+import cx from 'classnames'
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap'
+
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <main className="main-content">
+    <section class="section py-100">
+      <div class="container">
+        <header class="section-header">
+          <small>Oops</small>
+          <h2>Page Not Found!</h2>
+          <hr />
+          <p class="lead">Seems you're looking for something that doesn't exist.</p>
+        </header>
+
+        <br />
+        <p class="text-center"><a class="btn btn-primary" href="index.html">Retrun home</a></p>
+
+      </div>
+    </section>
+
+
+  </main>
 )
 
 export default NotFoundPage
