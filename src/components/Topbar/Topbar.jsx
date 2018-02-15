7271840841405067bcf1757e8370b7ab0c63794b
Casper Fabricius <me@casperfabricius.com>
2018-02-15 11:07:26 +0100

    Add body-scrolled class to body when window is scrolled. Since this only applies to the Topbar, this code was added to the Topbar component.

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import {
   Button,
@@ -11,31 +11,65 @@ import {
 import logoSrc from '../../assets/cimmerse_logo.svg'
 import styles from './Topbar.module.scss'
 
-const Topbar = () => (
-  <nav className="topbar topbar-inverse topbar-expand-md topbar-sticky">
-    <Container>
+class Topbar extends Component {
+  constructor(props) {
+    super(props)
+    this.handleScroll = this.handleScroll.bind(this)
+  }
 
-      <div className="topbar-left">
-        <button className="topbar-toggler">&#9776;</button>
-        <a className="topbar-brand" href="index.html">
-          <img className="logo-default" src={logoSrc} alt="logo" />
-          <img className="logo-inverse" src={logoSrc} alt="logo" />
-        </a>
-      </div>
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  render () {
+    return (
+      <nav className="topbar topbar-inverse topbar-expand-md topbar-sticky">
+        <Container>
+
+          <div className="topbar-left">
+            <button className="topbar-toggler">&#9776;</button>
+            <a className="topbar-brand" href="index.html">
+              <img className="logo-default" src={logoSrc} alt="logo" />
+              <img className="logo-inverse" src={logoSrc} alt="logo" />
+            </a>
+          </div>
 
 
-      <div className="topbar-right">
-        <Nav className="topbar-nav">
-          <NavItem><NavLink tag={Link} to='/'>Home</NavLink></NavItem>
-          <NavItem><NavLink tag={Link} to='/features'>Features</NavLink></NavItem>
-          <NavItem><NavLink tag={Link} to='/pricing'>Pricing</NavLink></NavItem>
-          <NavItem><NavLink tag={Link} to='/cases'>Cases</NavLink></NavItem>
-          <NavItem><Button color='primary' className={styles.getStartedButton}>Get started</Button></NavItem>
-        </Nav>
-      </div>
+          <div className="topbar-right">
+            <Nav className="topbar-nav">
+              <NavItem><NavLink tag={Link} to='/'>Home</NavLink></NavItem>
+              <NavItem><NavLink tag={Link} to='/features'>Features</NavLink></NavItem>
+              <NavItem><NavLink tag={Link} to='/pricing'>Pricing</NavLink></NavItem>
+              <NavItem><NavLink tag={Link} to='/cases'>Cases</NavLink></NavItem>
+              <NavItem><Button color='primary' className={styles.getStartedButton}>Get started</Button></NavItem>
+            </Nav>
+          </div>
 
-    </Container>
-  </nav>
-)
+        </Container>
+      </nav>
+    )
+  }
+
+  handleScroll () {
+    const body = document.body
+    if ( !body || !body.classList ) { return }
+
+    const currentScroll = this.getScrollTop()
+    if(currentScroll > 10) {
+      body.classList.add('body-scrolled')
+    } else {
+      body.classList.remove('body-scrolled')
+    }
+  }
+
+  getScrollTop () {
+    return window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop || 0
+  }
+}
 
 export default Topbar
