38e6268aa00af7dd4ba36b54e33c21e5ae240032
Casper Fabricius <me@casperfabricius.com>
2018-02-15 11:30:01 +0100

    Renamed page-2 to features page

--- src/pages/features.jsx
+++ src/pages/features.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
-  <div>
+const FeaturesPage = () => (
+  <main className="main-content">
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </div>
+  </main>
 )
 
-export default SecondPage
+export default FeaturesPage
