d388040a7dbc817ac0e2ccb115ce16d15d26543f
Casper Fabricius <me@casperfabricius.com>
2018-02-15 16:12:00 +0100

    Added Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-W62CRPZ',
+        // Include GTM in development.
+        includeInDevelopment: true
+      },
+    }
   ],
 };
