b837ba5330dc4c3d55ec13af78970d24007badbe
Casper Fabricius <me@casperfabricius.com>
2018-02-18 16:20:20 +0100

    Added missing keys to make react happy

--- src/components/Footer/FooterLinks.jsx
+++ src/components/Footer/FooterLinks.jsx
@@ -6,11 +6,11 @@ import {
 } from 'reactstrap'
 
 const FooterLinks = () => [
-  <NavItem><NavLink tag={Link} to='/about'>About</NavLink></NavItem>,
-  <NavItem><NavLink tag={Link} to='/contact'>Contact</NavLink></NavItem>,
-  <NavItem><NavLink href="https://embed.cimmerse.com/v2/integration_guide.pdf" target="blank">Documentation</NavLink></NavItem>,
-  <NavItem><NavLink tag={Link} to='/privacy' className='hidden-sm-down'>Privacy policy</NavLink></NavItem>,
-  <NavItem><NavLink tag={Link} to='/terms' className='hidden-sm-down'>Terms of use</NavLink></NavItem>
+  <NavItem key="about"><NavLink tag={Link} to='/about'>About</NavLink></NavItem>,
+  <NavItem key="contact"><NavLink tag={Link} to='/contact'>Contact</NavLink></NavItem>,
+  <NavItem key="docs"><NavLink href="https://embed.cimmerse.com/v2/integration_guide.pdf" target="blank">Documentation</NavLink></NavItem>,
+  <NavItem key="privacy"><NavLink tag={Link} to='/privacy' className='hidden-sm-down'>Privacy policy</NavLink></NavItem>,
+  <NavItem key="terms"><NavLink tag={Link} to='/terms' className='hidden-sm-down'>Terms of use</NavLink></NavItem>
 ]
 
 export default FooterLinks
