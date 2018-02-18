a68e1c30f19477ed4c627556e03b226035ae1605
Casper Fabricius <me@casperfabricius.com>
2018-02-18 13:35:11 +0100

    Added support for custom text on Get Started Button

--- src/components/GetStartedButton/GetStartedButton.jsx
+++ src/components/GetStartedButton/GetStartedButton.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Button } from 'reactstrap'
 
-const GetStartedButton = () => (
+const propTypes = {
+  buttonText: PropTypes.string
+}
+
+const GetStartedButton = ({ buttonText }) => (
   <Button
     color='primary'
     size='sm'
     onClick={() => { document.getElementById('getStartedButton').click() }}
-  >Get started</Button>
+  >{ buttonText || 'Get started' }</Button>
 )
+
+GetStartedButton.propTypes = propTypes
 
 export default GetStartedButton
