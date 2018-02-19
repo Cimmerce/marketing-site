622ad2228d270330cfd0deea2ea9504c3bc34012
Casper Fabricius <me@casperfabricius.com>
2018-02-19 15:44:06 +0100

    Fixed invalid react props in video modal

--- src/components/VideoModal/VideoModal.jsx
+++ src/components/VideoModal/VideoModal.jsx
@@ -16,12 +16,12 @@ const VideoModal = ({ isOpen, onToggle }) => {
   if(!isOpen) { return null }
 
   return (
-    <div className={cx(styles.lity, styles.lityOpened, styles.lityVimeo)} role="dialog" aria-label="Dialog Window (Press escape to close)" tabindex="-1" aria-hidden="false">
+    <div className={cx(styles.lity, styles.lityOpened, styles.lityVimeo)} role="dialog" aria-label="Dialog Window (Press escape to close)" tabIndex="-1" aria-hidden="false">
       <div className={styles.lityWrap} data-lity-close="" role="document">
         <div className={styles.lityContainer}>
           <div className={styles.lityContent}>
             <div className={styles.lityIframeContainer}>
-              <iframe frameborder="0" allowfullscreen="" src="https://player.vimeo.com/video/236880208?color=ffffff&autoplay=1"></iframe>
+              <iframe frameBorder="0" allowFullScreen src="https://player.vimeo.com/video/236880208?color=ffffff&autoplay=1"></iframe>
             </div>
           </div>
           <button className={styles.lityClose} type="button" aria-label="Close (Press escape to close)" onClick={onToggle}>×</button>
